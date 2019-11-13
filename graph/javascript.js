a5f4ac6a71ddb050161e9887d67bb8f02e0920c0
JokerS <43026670+HanksJR@users.noreply.github.com>
2019-11-13 21:28:24 +0700

    Update javascript.js

--- graph/javascript.js
+++ graph/javascript.js
@@ -2,11 +2,22 @@ var ctx = document.getElementById("myChart");
 var myRadarChart = new Chart(ctx, {
     type: 'radar',
     data: {
-        labels: ["Ammo", "Range", "Weight"],
+        labels: ["Ammo", "Range", "power(*100 FPS)"],
         datasets: [
             {
-            data: [1,10,50]
-            }
+            label:'ปืนสั้นอัดแก๊ส',
+            data: [1,10,4.3],
+            backgroundColor:'rgba(255, 0, 0, 0.1)',
+            borderColor:'rgba(255, 165, 0, 0.5)',
+            borderJoinStyle:'round'
+            },{
+            label:'ปืนสั้นมาตรฐาน',
+            data: [5,25,10],
+            backgroundColor:'rgba(0, 255, 0, 0.1)',
+            borderColor:'rgba(50, 205, 50, 0.5)',
+            borderJoinStyle:'round'
+            },
+
         ],
     }}
     );
